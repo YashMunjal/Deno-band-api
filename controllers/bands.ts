e7c0262bb307fc36be7bfccdb1d17a3186eb1ac9
yashmunjal <yashmu45@gmail.com>
2020-06-25 00:58:32 +0530

    oak done

--- controllers/bands.ts
+++ controllers/bands.ts
@@ -1,7 +1,35 @@
 import {Band} from '../types.ts';
+import {v4} from "https://deno.land/std/uuid/mod.ts";
 
-let bands:Band[];
+let bands:Band[]=[];
 
-const getBand = ({response}:{response})=>{
-    
+const getBands = ({response}:{response:any})=>{
+    response.body={
+        success: true,
+        data: bands,
+
+    }
 }
+
+const addBand = async ({request,response}:{request:any,response:any})=>{
+    const body = await request.body();
+    if(!request.hasBody){
+        response.status=400;
+        response.body= {
+            success:false,
+            msg:'The request is empty'
+        }
+    }
+    else{
+        const band:Band=body.value;
+        band.id=v4.generate();
+        bands.push(band);
+        response.status=201;
+        response.body= {
+            success:true,
+            msg:band
+        }
+    }
+}
+
+export {getBands,addBand};
--- server.ts
+++ server.ts
@@ -1,13 +1,17 @@
 import {Application,Router } from  "https://deno.land/x/oak/mod.ts";
+import {getBands,addBand} from "./controllers/bands.ts";
 
 const app = new Application();
 const PORT = 8080;
 const router = new Router();
 
 router
-    .get("/bands",null)
-    .post("/bands",null);
+    .get("/bands",getBands)
+    .post("/bands",addBand);
 
+
+
+app.use(router.routes());
 console.log(`App started on ${PORT}`);
 
 await app.listen({port:PORT});
